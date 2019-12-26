09ca118353fd89ff18202cdbe4d26b7a92b63146
Jun-Hyuk Woo <woojh3690@naver.com>
2019-12-26 10:04:18 +0900

    init

---
+++ DynamicMatrix/DynamicMatrix.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include "Matrix.h"
+
+int main()
+{
+	for (int i = 0; i < 1000; i++)
+	{
+		Matrix<int> test(vector<int> {10, 10});
+		test[0] = 1;
+		test[0][1] = 2;
+
+		Matrix<int> copyTest = test;
+		std::cout << copyTest[0][1] << std::endl;
+		std::cout << "Hello World! : " << i << std::endl;
+	}
+}
