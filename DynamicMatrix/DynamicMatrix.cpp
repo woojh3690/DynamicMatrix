9705e7400a8aa0fc9c62d1cadefa71350257fa6c
Jun-Hyuk Woo <woojh3690@naver.com>
2020-03-24 13:02:44 +0900

    브로드캐스팅을 양쪽에서도 지원할수게 수정

--- DynamicMatrix/DynamicMatrix.cpp
+++ DynamicMatrix/DynamicMatrix.cpp
@@ -12,11 +12,14 @@ int main()
 
 	Tensor<double> temp1({ 2, 2 }, 2);
 	Tensor<double> temp2({ 2, 2 }, 3);
+	Tensor<double> temp3({ 1 }, 4);
 
 	Tensor<double> result1 = 0.01 + temp1;
 	Tensor<bool> result2 = temp1 > temp2;
 	Tensor<double> result3 = temp1 * temp2;
 	Tensor<double> result4 = temp1.matmul(temp2);
+
+	Tensor<double> result5 = temp3 * temp1;
 
 	printf("end");
 } 
--- DynamicMatrix/OperatorMacro.h
+++ DynamicMatrix/OperatorMacro.h
@@ -1,26 +1,10 @@
 #ifndef _TENSOR_OPERATORMACRO_H_
 #define _TENSOR_OPERATORMACRO_H_
 
-#define MAKE_TENSOR_OPERATOR(METHOD) \
-auto& operator##METHOD##(Tensor<double>& lTsr, Tensor<double>& rTsr) \
-{ \
-	auto type = 0.1 ##METHOD## 0.1; \
- \
-	if (lTsr.shape() != rTsr.shape()) \
-	{ \
-		rTsr = rTsr.broadcasting(lTsr.shape()); \
-	} \
- \
-	Tensor<decltype(type)>* result = new Tensor<decltype(type)>(lTsr.shape()); \
-	for (int i = 0; i < lTsr.volume(); i++) \
-	{ \
-		vector<int> idx = lTsr.changeIdxOfDim(i); \
-		double tsrValue = lTsr.operator[](idx).value(); \
-		double value = rTsr.operator[](idx).value(); \
-		result->operator[](idx) = tsrValue ##METHOD## value; \
-	} \
-	return *result; \
-}
+#define DEFINE_OPERATOR(METHOD) \
+friend auto& operator##METHOD##(Tensor<double> lTsr, Tensor<double> rTsr); \
+friend auto& operator##METHOD##(Tensor<double>& lTsr, double value); \
+friend auto& operator##METHOD##(double value, Tensor<double>& lTsr); \
 
 #define MAKE_RIGHT_OPERATOR_DOUBLE(METHOD) \
 auto& operator##METHOD##(Tensor<double>& lTsr, double value) \
@@ -50,15 +34,47 @@ auto& operator##METHOD##(double value, Tensor<double>& lTsr) \
 	return *result; \
 }
 
-#define MAKE_OPERATOR(METHOD) \
-MAKE_TENSOR_OPERATOR(METHOD) \
-MAKE_RIGHT_OPERATOR_DOUBLE(METHOD) \
-MAKE_LEFT_OPERATOR_DOUBLE(METHOD)
+#define MAKE_TENSOR_OPERATOR(METHOD) \
+auto& operator##METHOD##(Tensor<double> lTsr, Tensor<double> rTsr) \
+{ \
+	vector<int> lShape = lTsr.shape(); \
+	vector<int> rShape = rTsr.shape(); \
+	if (lShape != rShape) \
+	{ \
+		if (lShape == vector<int>({1})) \
+		{ \
+			return operator##METHOD##(lTsr[0].value(), rTsr); \
+		} \
+		else if (rShape == vector<int>({ 1 })) \
+		{ \
+			return operator##METHOD##(lTsr, rTsr[0].value()); \
+		} \
+		else if (lShape.size() > rShape.size()) \
+		{ \
+			rTsr = rTsr.broadcasting(lShape); \
+		} \
+		else if (lShape.size() < rShape.size())\
+		{ \
+			lTsr = lTsr.broadcasting(rShape); \
+		} \
+	} \
+ \
+	auto type = 0.1 ##METHOD## 0.1; \
+	Tensor<decltype(type)>* result = new Tensor<decltype(type)>(lTsr.shape()); \
+	for (int i = 0; i < lTsr.volume(); i++) \
+	{ \
+		vector<int> idx = lTsr.changeIdxOfDim(i); \
+		double tsrValue = lTsr.operator[](idx).value(); \
+		double value = rTsr.operator[](idx).value(); \
+		result->operator[](idx) = tsrValue ##METHOD## value; \
+	} \
+	return *result; \
+}
 
-#define DEFINE_OPERATOR(METHOD) \
-friend auto& operator##METHOD##(Tensor<double>& lTsr, Tensor<double>& rTsr); \
-friend auto& operator##METHOD##(Tensor<double>& lTsr, double value); \
-friend auto& operator##METHOD##(double value, Tensor<double>& lTsr); \
+#define MAKE_OPERATOR(METHOD) \
+MAKE_RIGHT_OPERATOR_DOUBLE(METHOD) \
+MAKE_LEFT_OPERATOR_DOUBLE(METHOD) \
+MAKE_TENSOR_OPERATOR(METHOD)
 
 #endif // !_TENSOR_OPERATORMACRO_H_
 
